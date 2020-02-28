952a4712707bc943951f97a080ced41d5084e936
tina hong <tkim826@yahoo.com>
2020-02-28 13:34:15 -0800

    simplified nav bar

--- assets/theme/js/theme.js
+++ assets/theme/js/theme.js
@@ -1,5 +1,5 @@
 (function($) {
-  'use strict';
+  "use strict";
 
   //Run function When Document Ready
   $(document).ready(function() {
@@ -20,9 +20,9 @@
   });
 
   //Run function When PACE (page loader) hide
-  Pace.on('hide', function() {
-    $('.wrapper')
-      .css('visibility', 'visible')
+  Pace.on("hide", function() {
+    $(".wrapper")
+      .css("visibility", "visible")
       .animate({ opacity: 1.0 }, 2000, function() {
         initCheckNav();
       });
@@ -30,7 +30,7 @@
     if (window.location.hash) {
       $('.link-inpage[href="' + window.location.hash + '"]')
         .first()
-        .trigger('click');
+        .trigger("click");
     }
   });
 
@@ -41,33 +41,33 @@
 
   // ajax contact form
   function initAjaxContactForm() {
-    if ($('#contactForm, #hireForm').length > 0) {
-      $('#contactForm, #hireForm').validate();
-      $('#contactForm, #hireForm').submit(function() {
+    if ($("#contactForm, #hireForm").length > 0) {
+      $("#contactForm, #hireForm").validate();
+      $("#contactForm, #hireForm").submit(function() {
         var el = $(this);
         if (el.valid()) {
           var params = $(this).serialize();
           $.ajax({
-            type: 'POST',
+            type: "POST",
             data: params,
-            url: 'php/sending_mail.php',
+            url: "php/sending_mail.php",
             beforeSend: function() {
-              el.find('.preload-submit').removeClass('hidden');
-              el.find('.message-submit').addClass('hidden');
+              el.find(".preload-submit").removeClass("hidden");
+              el.find(".message-submit").addClass("hidden");
             },
             success: function(res) {
               res = jQuery.parseJSON(res);
               setTimeout(function() {
-                el.find('.preload-submit').addClass('hidden');
+                el.find(".preload-submit").addClass("hidden");
                 if (res.error === null) {
-                  el.trigger('reset');
-                  el.find('.message-submit')
+                  el.trigger("reset");
+                  el.find(".message-submit")
                     .html(res.msg)
-                    .removeClass('hidden');
+                    .removeClass("hidden");
                 } else {
-                  el.find('.message-submit')
+                  el.find(".message-submit")
                     .html(res.error)
-                    .removeClass('hidden');
+                    .removeClass("hidden");
                 }
               }, 1000);
             }
@@ -80,38 +80,38 @@
 
   // ajax Uploader file
   function initAjaxUploader() {
-    if ($('#upload-btn').length > 0) {
-      var btn = document.getElementById('upload-btn'),
-        wrap = document.getElementById('pic-progress-wrap'),
-        picBox = document.getElementById('picbox'),
-        errBox = document.getElementById('errormsg');
+    if ($("#upload-btn").length > 0) {
+      var btn = document.getElementById("upload-btn"),
+        wrap = document.getElementById("pic-progress-wrap"),
+        picBox = document.getElementById("picbox"),
+        errBox = document.getElementById("errormsg");
 
       var uploader = new ss.SimpleUpload({
         button: btn,
-        url: 'php/upload.php',
+        url: "php/upload.php",
         progressUrl:
-          'assets/plugins/Simple-Ajax-Uploader/extras/uploadProgress.php',
-        name: 'fileatt',
+          "assets/plugins/Simple-Ajax-Uploader/extras/uploadProgress.php",
+        name: "fileatt",
         multiple: false,
         maxUploads: 2,
         maxSize: 200,
         queue: false,
-        allowedExtensions: ['pdf'],
+        allowedExtensions: ["pdf"],
         debug: true,
-        hoverClass: 'btn-hover',
-        focusClass: 'active',
-        disabledClass: 'disabled',
-        responseType: 'json',
+        hoverClass: "btn-hover",
+        focusClass: "active",
+        disabledClass: "disabled",
+        responseType: "json",
         onSubmit: function(filename, ext) {
-          var prog = document.createElement('div'),
-            outer = document.createElement('div'),
-            bar = document.createElement('div'),
-            size = document.createElement('div'),
+          var prog = document.createElement("div"),
+            outer = document.createElement("div"),
+            bar = document.createElement("div"),
+            size = document.createElement("div"),
             self = this;
-          prog.className = 'prog';
-          size.className = 'size';
-          outer.className = 'progress';
-          bar.className = 'bar';
+          prog.className = "prog";
+          size.className = "size";
+          outer.className = "progress";
+          bar.className = "bar";
 
           outer.appendChild(bar);
           prog.appendChild(size);
@@ -122,13 +122,13 @@
           self.setProgressContainer(prog);
           self.setFileSizeBox(size);
 
-          errBox.innerHTML = '';
+          errBox.innerHTML = "";
         },
         onSizeError: function(filename, fileSize) {
-          errBox.innerHTML = 'Max size 200K';
+          errBox.innerHTML = "Max size 200K";
         },
         onExtError: function(filename, extension) {
-          errBox.innerHTML = 'File extension not permitted';
+          errBox.innerHTML = "File extension not permitted";
         },
         onError: function(
           filename,
@@ -142,17 +142,17 @@
         },
         onComplete: function(file, response) {
           if (!response) {
-            errBox.innerHTML = 'Unable to upload file';
+            errBox.innerHTML = "Unable to upload file";
           }
           if (response.success === true) {
             picBox.innerHTML =
               '<i class="fa fa-file-pdf-o"></i> &nbsp;' + response.file;
-            $('#file-att').val(response.file);
+            $("#file-att").val(response.file);
           } else {
             if (response.msg) {
               errBox.innerHTML = response.msg;
             } else {
-              errBox.innerHTML = 'Unable to upload file';
+              errBox.innerHTML = "Unable to upload file";
             }
           }
         }
@@ -162,38 +162,38 @@
 
   //captcha configuration
   function initCaptcha() {
-    $('#mycaptcha').simpleCaptcha({
+    $("#mycaptcha").simpleCaptcha({
       allowRefresh: false,
-      scriptPath: 'assets/plugins/simpleCaptcha/simpleCaptcha.php'
+      scriptPath: "assets/plugins/simpleCaptcha/simpleCaptcha.php"
     });
 
-    $('#mycaptcha').bind('ready.simpleCaptcha', function(hashSelected) {
-      $('#captcha1,#captcha2')
-        .html($('#mycaptcha-wrap').html())
-        .find('.mycaptcha1')
-        .removeAttr('id');
-      $('#captcha1,#captcha2')
-        .find('.captchaImages img.captchaImage')
+    $("#mycaptcha").bind("ready.simpleCaptcha", function(hashSelected) {
+      $("#captcha1,#captcha2")
+        .html($("#mycaptcha-wrap").html())
+        .find(".mycaptcha1")
+        .removeAttr("id");
+      $("#captcha1,#captcha2")
+        .find(".captchaImages img.captchaImage")
         .click(function() {
-          $('#captcha1,#captcha2')
-            .find('.captchaImages img.captchaImage')
-            .removeClass('simpleCaptchaSelected');
-          $(this).addClass('simpleCaptchaSelected');
-          $('.simpleCaptchaInput').val($(this).data('hash'));
+          $("#captcha1,#captcha2")
+            .find(".captchaImages img.captchaImage")
+            .removeClass("simpleCaptchaSelected");
+          $(this).addClass("simpleCaptchaSelected");
+          $(".simpleCaptchaInput").val($(this).data("hash"));
         });
     });
   }
 
   //Typed Animation
   function initTyped() {
-    $('#typed').typed({
-      strings: ['Adaptable', 'Curious', 'UX Designer'],
+    $("").typed({
+      strings: [],
       // typing speed
-      typeSpeed: 100,
+      typeSpeed: 50,
       // time before typing starts
       startDelay: 100,
       // backspacing speed
-      backSpeed: 35,
+      backSpeed: 10,
       // time before backspacing
       backDelay: 1000,
       // loop
@@ -203,11 +203,11 @@
       // show cursor
       showCursor: true,
       // character for cursor
-      cursorChar: '.',
+      cursorChar: ".",
       // attribute to type (null == text)
       attr: null,
       // either html or text
-      contentType: 'html',
+      contentType: "html",
       // call when done callback function
       callback: function() {},
       // starting callback function before each string
@@ -221,11 +221,11 @@
 
   //Lightbox (popup)
   function initLightBox() {
-    $('.list-work').magnificPopup({
-      delegate: 'a.galery-item',
-      type: 'image',
-      tLoading: 'Loading image #%curr%...',
-      mainClass: 'mfp-img-mobile',
+    $(".list-work").magnificPopup({
+      delegate: "a.galery-item",
+      type: "image",
+      tLoading: "Loading image #%curr%...",
+      mainClass: "mfp-img-mobile",
       gallery: {
         enabled: true,
         navigateByImgClick: true,
@@ -234,15 +234,15 @@
       image: {
         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
         titleSrc: function(item) {
-          return item.el.attr('title');
+          return item.el.attr("title");
         }
       }
     });
 
-    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
       disableOn: 700,
-      type: 'iframe',
-      mainClass: 'mfp-fade',
+      type: "iframe",
+      mainClass: "mfp-fade",
       removalDelay: 160,
       preloader: false,
       fixedContentPos: false
@@ -251,12 +251,12 @@
 
   //Chart
   function initEasyChart() {
-    $('.chart').easyPieChart({
-      easing: 'easeOutBounce',
-      barColor: '#000',
+    $(".chart").easyPieChart({
+      easing: "easeOutBounce",
+      barColor: "#000",
       onStep: function(from, to, percent) {
         $(this.el)
-          .find('.percent')
+          .find(".percent")
           .text(Math.round(percent));
       }
     });
@@ -264,44 +264,44 @@
 
   //Click Envents
   function initClickedEvents() {
-    $('#hireme-tab').click(function() {
-      $('#myTab a[href="#tab1"]').tab('show');
+    $("#hireme-tab").click(function() {
+      $('#myTab a[href="#tab1"]').tab("show");
     });
 
-    $('#contact-tab').click(function() {
-      $('#myTab a[href="#tab0"]').tab('show');
+    $("#contact-tab").click(function() {
+      $('#myTab a[href="#tab0"]').tab("show");
     });
 
-    $('.map-area').click(function() {
-      $(this).addClass('show');
+    $(".map-area").click(function() {
+      $(this).addClass("show");
     });
 
-    $('.back-to-top').click(function() {
-      $('html, body')
+    $(".back-to-top").click(function() {
+      $("html, body")
         .stop()
         .animate(
           {
             scrollTop: 0
           },
           1500,
-          'easeInOutExpo',
+          "easeInOutExpo",
           function() {}
         );
       return false;
     });
 
-    $('.link-inpage').click(function(e) {
+    $(".link-inpage").click(function(e) {
       var target = this.hash,
         $target = $(target);
-      $('html, body')
+      $("html, body")
         .stop()
         .animate(
           {
             scrollTop:
-              $target.offset().top - ($('.menu-area').outerHeight() - 1)
+              $target.offset().top - ($(".menu-area").outerHeight() - 1)
           },
           1500,
-          'easeInOutExpo',
+          "easeInOutExpo",
           function() {
             //window.location.hash = target;
           }
@@ -312,12 +312,12 @@
 
   //Navbar Scroll
   function initNavbarSrcoll() {
-    if ($('.main-header').length > 0) {
+    if ($(".main-header").length > 0) {
       var mainbottom =
-        $('.main-header').offset().top + $('.main-header').height();
-      $(window).on('scroll', function() {
+        $(".main-header").offset().top + $(".main-header").height();
+      $(window).on("scroll", function() {
         var stopWindow =
-          Math.round($(window).scrollTop()) + $('.menu-area').outerHeight();
+          Math.round($(window).scrollTop()) + $(".menu-area").outerHeight();
         conditionNavbar(stopWindow, mainbottom);
       });
     }
@@ -325,11 +325,11 @@
 
   //Check Navar Show
   function initCheckNav() {
-    if ($('.main-header').length > 0) {
+    if ($(".main-header").length > 0) {
       var mainbottom =
-        $('.main-header').offset().top + $('.main-header').height();
+        $(".main-header").offset().top + $(".main-header").height();
       var stopWindow =
-        Math.round($(window).scrollTop()) + $('.menu-area').outerHeight();
+        Math.round($(window).scrollTop()) + $(".menu-area").outerHeight();
       conditionNavbar(stopWindow, mainbottom);
     }
   }
@@ -337,34 +337,34 @@
   //Condition Navbar
   function conditionNavbar(stopWindow, mainbottom) {
     if (stopWindow > mainbottom) {
-      $('.menu-area').addClass('nav-fixed');
+      $(".menu-area").addClass("nav-fixed");
     } else {
-      $('.menu-area').removeClass('nav-fixed nav-white-bg');
+      $(".menu-area").removeClass("nav-fixed nav-white-bg");
     }
-    if (stopWindow > $('.menu-area').outerHeight()) {
-      $('.menu-area').addClass('nav-white-bg');
+    if (stopWindow > $(".menu-area").outerHeight()) {
+      $(".menu-area").addClass("nav-white-bg");
     }
   }
 
   //Bg Parallax
   function initParallax() {
-    $('.parallax-bg').each(function() {
-      $(this).parallax('50%', 0.3);
+    $(".parallax-bg").each(function() {
+      $(this).parallax("50%", 0.3);
     });
   }
 
   //Set header to window
   function initGetHWindow() {
     var wHeight = $(window).height();
-    if (wHeight > 600 && !$('.main-header').hasClass('no-window')) {
-      $('.main-header, .header-content-fixed').height(wHeight);
+    if (wHeight > 600 && !$(".main-header").hasClass("no-window")) {
+      $(".main-header, .header-content-fixed").height(wHeight);
     }
   }
 
   //Map
   function initMap() {
-    $('#map-contact').gmap({
-      center: '-6.600000, 106.800000',
+    $("#map-contact").gmap({
+      center: "-6.600000, 106.800000",
       zoom: 15,
       scrollwheel: false,
       disableDefaultUI: false,
@@ -372,13 +372,13 @@
         var self = this;
         self
           .addMarker({
-            position: this.get('map').getCenter(),
-            icon: 'assets/theme/images/marker.png'
+            position: this.get("map").getCenter(),
+            icon: "assets/theme/images/marker.png"
           })
           .click(function() {
             self.openInfoWindow(
               {
-                content: $('.map-contact-body').html()
+                content: $(".map-contact-body").html()
               },
               this
             );
@@ -388,13 +388,13 @@
   }
 
   function initHold() {
-    $('[data-holdwidth]').each(function(index, el) {
-      var width = $(el).data('holdwidth');
-      $(el).css('width', width);
+    $("[data-holdwidth]").each(function(index, el) {
+      var width = $(el).data("holdwidth");
+      $(el).css("width", width);
     });
-    $('[data-holdbg]').each(function(index, el) {
-      var bg = $(el).data('holdbg');
-      $(el).css('background-image', 'url(' + bg + ')');
+    $("[data-holdbg]").each(function(index, el) {
+      var bg = $(el).data("holdbg");
+      $(el).css("background-image", "url(" + bg + ")");
     });
   }
 
@@ -405,21 +405,21 @@
 
   //Tigger Custom Btn FIle
   function initBtnFile() {
-    $(document).on('change', '.btn-file :file', function() {
+    $(document).on("change", ".btn-file :file", function() {
       var input = $(this),
         numFiles = input.get(0).files ? input.get(0).files.length : 1,
         label = input
           .val()
-          .replace(/\\/g, '/')
-          .replace(/.*\//, '');
-      input.trigger('fileselect', [numFiles, label]);
+          .replace(/\\/g, "/")
+          .replace(/.*\//, "");
+      input.trigger("fileselect", [numFiles, label]);
     });
 
-    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+    $(".btn-file :file").on("fileselect", function(event, numFiles, label) {
       var input = $(this)
-          .parents('.input-group')
-          .find(':text'),
-        log = numFiles > 1 ? numFiles + ' files selected' : label;
+          .parents(".input-group")
+          .find(":text"),
+        log = numFiles > 1 ? numFiles + " files selected" : label;
       if (input.length) {
         input.val(log);
       } else {
